7652a91a9dc8d42a81b167be2623c4ea80e2e9dd
Adam Dangoor <adamdangoor@gmail.com>
2026-03-17 11:39:33 +0000

    Isolate dotnet HOME per invocation to fix NuGet mutex conflict

--- check_csharp_golden_syntax.py
+++ check_csharp_golden_syntax.py
@@ -19,6 +19,7 @@ def _target_framework(dotnet_path: str) -> str:
         env.update(
             {
                 "TMPDIR": dotnet_tmp.as_posix(),
+                "HOME": dotnet_tmp.as_posix(),
                 "DOTNET_SKIP_FIRST_TIME_EXPERIENCE": "1",
                 "DOTNET_NOLOGO": "1",
             }
@@ -61,9 +62,10 @@ def main() -> None:
             dotnet_tmp = Path(tmpdir) / "tmp"
             dotnet_tmp.mkdir()
             env = os.environ.copy()
-            env["TMPDIR"] = dotnet_tmp.as_posix()
             env.update(
                 {
+                    "TMPDIR": dotnet_tmp.as_posix(),
+                    "HOME": dotnet_tmp.as_posix(),
                     "DOTNET_SKIP_FIRST_TIME_EXPERIENCE": "1",
                     "DOTNET_NOLOGO": "1",
                 }
