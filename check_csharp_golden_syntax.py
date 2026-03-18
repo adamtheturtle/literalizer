0cccd10b544c33da619a5c8ec6859c73f603774c
Adam Dangoor <adamdangoor@gmail.com>
2026-03-18 12:57:19 +0000

    Revert C# checker change; track follow-up in #256

--- check_csharp_golden_syntax.py
+++ check_csharp_golden_syntax.py
@@ -54,14 +54,9 @@ def main() -> None:
             f"    <TargetFramework>{target_framework}</TargetFramework>\n"
             "    <Nullable>disable</Nullable>\n"
             "    <ImplicitUsings>enable</ImplicitUsings>\n"
-            "    <EnableDefaultCompileItems>false"
-            "</EnableDefaultCompileItems>\n"
             "    <EnableDefaultEmbeddedResourceItems>false"
             "</EnableDefaultEmbeddedResourceItems>\n"
             "  </PropertyGroup>\n"
-            "  <ItemGroup>\n"
-            '    <Compile Include="Program.cs" />\n'
-            "  </ItemGroup>\n"
             "</Project>\n"
         )
 
