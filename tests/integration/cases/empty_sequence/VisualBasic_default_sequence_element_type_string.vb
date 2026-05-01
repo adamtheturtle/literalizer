deda95fd01455910a1318885c111bf91cd97e5e4
Adam Dangoor <adamdangoor@gmail.com>
2026-05-01 21:47:05 +0100

    Fix VB default sequence fallback

--- tests/integration/cases/empty_sequence/VisualBasic_default_sequence_element_type_string.vb
+++ tests/integration/cases/empty_sequence/VisualBasic_default_sequence_element_type_string.vb
@@ -1,6 +1,6 @@
 Imports System.Collections.Generic
 Module Check
-    Dim my_data = New String() {
+    Dim my_data = New Object() {
         New String() {},
         New Dictionary(Of String, Object) From {}
     }
--- tests/integration/cases/simple_sequence/VisualBasic_default_sequence_element_type_string.vb
+++ tests/integration/cases/simple_sequence/VisualBasic_default_sequence_element_type_string.vb
@@ -1,6 +1,6 @@
 Imports System.Collections.Generic
 Module Check
-    Dim my_data = New String() {
+    Dim my_data = New Object() {
         1,
         "hello",
         True,
