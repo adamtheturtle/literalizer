2c26a616c9337dcf0e49277b9dba9b90920d1aed
Adam Dangoor <adamdangoor@gmail.com>
2026-08-10 23:53:30 +0100

    Regenerate combined Visual Basic date goldens

--- tests/integration/cases/date_list/VisualBasic_combined@dotnet_6.vb
+++ tests/integration/cases/date_list/VisualBasic_combined@dotnet_6.vb
@@ -1,14 +1,14 @@
 Imports System.Collections.Generic
 Module Check
     Sub _declaration()
-        Dim my_data = New String() {
+        Dim my_data = New DateOnly() {
             New DateOnly(2024, 1, 15),
             New DateOnly(2024, 2, 20)
         }
     End Sub
     Sub _assignment()
         Dim my_data As Object
-        my_data = New String() {
+        my_data = New DateOnly() {
             New DateOnly(2024, 1, 15),
             New DateOnly(2024, 2, 20)
         }
--- tests/integration/cases/date_set/VisualBasic_combined@dotnet_6.vb
+++ tests/integration/cases/date_set/VisualBasic_combined@dotnet_6.vb
@@ -1,13 +1,13 @@
 Module Check
     Sub _declaration()
-        Dim my_data = New HashSet(Of String) From {
+        Dim my_data = New HashSet(Of DateOnly) From {
             New DateOnly(2024, 1, 15),
             New DateOnly(2024, 6, 1)
         }
     End Sub
     Sub _assignment()
         Dim my_data As Object
-        my_data = New HashSet(Of String) From {
+        my_data = New HashSet(Of DateOnly) From {
             New DateOnly(2024, 1, 15),
             New DateOnly(2024, 6, 1)
         }
