54beedba1e11e2d20359c1063949c78a4a85ffd5
Adam Dangoor <adamdangoor@gmail.com>
2026-08-11 07:33:23 +0100

    Refresh shared golden fixtures

--- tests/integration/cases/record_keyword_field/Swift_record_keyword_field@v5_9.swift
+++ tests/integration/cases/record_keyword_field/Swift_record_keyword_field@v5_9.swift
@@ -1,4 +1,4 @@
 struct Record0 { let type: String; let match: String; let error: String; let `switch`: String; let `class`: String; let int: String; let new: String; let `static`: String; let fun: String; let object: String; let val: String; let when: String; let `func`: String; let `let`: String; let `var`: String; let template: String; let id: Int }
-let my_data: Any = [
+let my_data = [
     Record0(type: "a", match: "b", error: "c", switch: "d", class: "e", int: "f", new: "g", static: "h", fun: "i", object: "j", val: "k", when: "l", func: "m", let: "n", var: "o", template: "p", id: 1),
 ]
