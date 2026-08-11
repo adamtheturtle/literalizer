7136f384f53667f47228c25127b11e364d16db75
Adam Dangoor <adamdangoor@gmail.com>
2026-08-11 11:56:04 +0100

    Refresh Perl round-trip goldens against main

---
+++ tests/integration/cases/call_ref_args_trivial_register/Perl_call@v5_36.pl
@@ -0,0 +1,14 @@
+use JSON::PP;
+sub process {}
+my $my_int = 1;
+my $my_bool = JSON::PP::true;
+my $my_float = (0.0 + 3.14);
+my $my_list = [
+    1,
+    2,
+    3,
+];
+process($my_int, 42);
+process($my_bool, 7);
+process($my_float, 9);
+process($my_list, 1);
