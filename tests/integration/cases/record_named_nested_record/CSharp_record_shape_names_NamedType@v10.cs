0c4126291b29beef0d1e47fd5d87ed0856949c7a
Adam Dangoor <adamdangoor@gmail.com>
2026-08-13 17:08:42 +0100

    Add C# record shape names

---
+++ tests/integration/cases/record_named_nested_record/CSharp_record_shape_names_NamedType@v10.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+record NamedType(int Id, string Label, bool Enabled, int[] RelatedIds);
+record Record0(string Collection, NamedType FeaturedEntry);
+class Check {
+    public static void Main() {
+var my_data = new Record0(
+    "alpha",
+    new NamedType(
+        100,
+        "first entry",
+        false,
+        new int[] {
+            102,
+            103
+        }
+    )
+);
+    }
+}
