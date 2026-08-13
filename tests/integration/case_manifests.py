e11004b9477b3361320cf6bbb182dcce4ffc6b01
Adam Dangoor <adamdangoor@gmail.com>
2026-08-13 21:25:04 +0100

    Move escaped ref coverage to goldens

--- tests/integration/case_manifests.py
+++ tests/integration/case_manifests.py
@@ -394,6 +394,7 @@ class RefCaseSpec(_OwnedCaseSpec, frozen=True):
     """
 
     ref_key: str = "$ref"
+    languages: StringFrozenSet = Field(default_factory=_empty_name_set)
     collection_layout: CollectionLayoutName = "compact"
     ref_case_override: RefIdentifierCase | None = None
     value_sources: dict[str, str] = Field(default_factory=_empty_sources)
