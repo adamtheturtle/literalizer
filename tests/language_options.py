ce59352bd18d29c74dcb52f3f124db5dee24541a
Adam Dangoor <adamdangoor@gmail.com>
2026-08-14 08:25:31 +0100

    Address special-float capability feedback

--- tests/language_options.py
+++ tests/language_options.py
@@ -391,12 +391,10 @@ CAPABILITY_FLAGS: Mapping[str, Callable[[literalizer.LanguageCls], bool]] = {
     "rejects_non_string_dict_keys": (
         lambda lang_cls: not lang_cls.supports_non_string_dict_keys
     ),
-    "supports_non_string_dict_keys": (
-        lambda lang_cls: lang_cls.supports_non_string_dict_keys
-    ),
     "supports_special_float_dict_keys": (
         lambda lang_cls: (
             lang_cls.supports_non_string_dict_keys
+            and lang_cls.supports_special_floats
             and lang_cls.language_id not in {"groovy", "lua", "rust"}
         )
     ),
