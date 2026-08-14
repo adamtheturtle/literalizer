a6fe3b7d410e44a6dd4f62aa597927984b06df99
Adam Dangoor <adamdangoor@gmail.com>
2026-08-14 08:47:19 +0100

    Remove non-TOML carriage-return test

--- tests/errors/test_call_errors.py
+++ tests/errors/test_call_errors.py
@@ -546,14 +546,7 @@ def test_literalize_call_comment_source_length_mismatch_raises() -> None:
         )
 
 
-@pytest.mark.parametrize(
-    argnames="line_break",
-    argvalues=["\n", "\r", "\r\n"],
-    ids=["line-feed", "carriage-return", "crlf"],
-)
-def test_literalize_call_comment_source_multiline_entry_raises(
-    line_break: str,
-) -> None:
+def test_literalize_call_comment_source_multiline_entry_raises() -> None:
     """A ``comment_source`` entry may not span multiple lines."""
     with pytest.raises(
         expected_exception=CommentSourceMultilineError,
@@ -568,7 +561,7 @@ def test_literalize_call_comment_source_multiline_entry_raises(
             language=Python(),
             target_function="process",
             parameter_names=["value"],
-            comment_source=["fine", f"broken{line_break}comment"],
+            comment_source=["fine", "broken\ncomment"],
         )
 
 
