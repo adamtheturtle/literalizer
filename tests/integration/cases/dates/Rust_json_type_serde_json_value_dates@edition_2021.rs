26e1322eaa0b77b04540e3b5a5e61e82138d1f01
Adam Dangoor <adamdangoor@gmail.com>
2026-08-25 15:49:15 +0100

    Raise Rust serde JSON recursion limit

--- tests/integration/cases/dates/Rust_json_type_serde_json_value_dates@edition_2021.rs
+++ tests/integration/cases/dates/Rust_json_type_serde_json_value_dates@edition_2021.rs
@@ -1,3 +1,5 @@
+// Permit serde_json::json! to expand wide values.
+#![recursion_limit = "4096"]
 fn main() {
     let my_data: serde_json::Value = serde_json::json!({
         "date": "2024-01-15",
--- tests/integration/cases/nested_mixed_inner/Rust_json_type_serde_json_value_nested_mixed@edition_2021.rs
+++ tests/integration/cases/nested_mixed_inner/Rust_json_type_serde_json_value_nested_mixed@edition_2021.rs
@@ -1,3 +1,5 @@
+// Permit serde_json::json! to expand wide values.
+#![recursion_limit = "4096"]
 fn main() {
     let my_data: serde_json::Value = serde_json::json!([
         serde_json::json!([1, "a"]),
