41f04bb745446f0ec5df74745d4dc8e4ec0fa784
Adam Dangoor <adamdangoor@gmail.com>
2026-08-30 10:47:50 +0100

    Fix Kotlin nested map list types

---
+++ tests/integration/cases/kotlin_list_of_maps_with_lists/Kotlin@v1_9.kts
@@ -0,0 +1,4 @@
+val my_data = listOf<Map<String, IntArray>>(
+    mapOf<String, IntArray>("a" to intArrayOf(1)),
+    mapOf<String, IntArray>("a" to intArrayOf(2)),
+)
---
+++ tests/integration/cases/kotlin_list_of_maps_with_lists/Kotlin_dict_format_hash_map@v1_9.kts
@@ -0,0 +1,4 @@
+val my_data = listOf<Map<String, IntArray>>(
+    hashMapOf<String, IntArray>("a" to intArrayOf(1)),
+    hashMapOf<String, IntArray>("a" to intArrayOf(2)),
+)
