8ce35aa801ef4e4b3dbc2cfb93ad75ce176bac51
Adam Dangoor <adamdangoor@gmail.com>
2026-08-31 21:13:37 +0100

    Keep empty Kotlin lists in array container family

--- tests/integration/cases/sibling_list_values_nested/Kotlin_combined@v1_9.kts
+++ tests/integration/cases/sibling_list_values_nested/Kotlin_combined@v1_9.kts
@@ -1,8 +1,8 @@
 var my_data = mapOf<String, Any?>(
-    "lint" to listOf<Any?>(2, listOf<Any?>()),
+    "lint" to listOf<Any?>(2, arrayOf<Any?>()),
     "test" to listOf<Any?>(5, arrayOf("compile")),
 )
 my_data = mapOf<String, Any?>(
-    "lint" to listOf<Any?>(2, listOf<Any?>()),
+    "lint" to listOf<Any?>(2, arrayOf<Any?>()),
     "test" to listOf<Any?>(5, arrayOf("compile")),
 )
