de27278892d72e2b1c386524fe7d8977d5dc30f8
Adam Dangoor <adamdangoor@gmail.com>
2026-09-10 00:45:03 +0100

    Replace implicit truthiness in test exception hierarchy

--- tests/errors/test_exception_hierarchy.py
+++ tests/errors/test_exception_hierarchy.py
@@ -17,7 +17,7 @@ def test_every_literalizer_exception_has_public_base() -> None:
         and member.__module__ == literalizer.exceptions.__name__
     ]
 
-    assert exception_classes  # pyrefly: ignore [implicit-bool]
+    assert len(exception_classes) > 0
     assert all(
         issubclass(exception_class, LiteralizerError)
         for exception_class in exception_classes
