6448a568969d4a405e26dcc3916120c51c866344
Adam Dangoor <adamdangoor@gmail.com>
2026-09-18 23:01:31 +0100

    Share opt-in record golden cases across languages

--- tests/integration/cases/opt_in_record_nested/Haskell_dict_format_record@ghc2021.hs
+++ tests/integration/cases/opt_in_record_nested/Haskell_dict_format_record@ghc2021.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DuplicateRecordFields #-}
-module Fixture_haskell_typed_record_nested_Haskell_dict_format_record where
+module Fixture_opt_in_record_nested_Haskell_dict_format_record where
 data Val1 = Val1 { name :: String, active :: Bool }
 data Val2 = Val2 { name :: String, score :: Double }
 data Val0 = Val0 { owner :: Val1, members :: [Val2] }
